ab7529cfb7d9e656095e45478bb94e1579d92917
Ibanheiz <nicolas.ibanheiz@gmail.com>
2015-08-25 13:37:29 -0300

    Externalizando API de conexão da base de endereço. <Modificar username e password no conexao.properties>

--- test/com/jovetecnologia/test/FreteCorreiosTest.java
+++ test/com/jovetecnologia/test/FreteCorreiosTest.java
@@ -1,19 +1,19 @@
-package com.jove.test;
+package com.jovetecnologia.test;
 
 import javax.swing.JOptionPane;
 
-import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class FreteCorreiosTest {
 
 	@Test
 	public void calcularFrete() {
 		String cepOrigem = JOptionPane.showInputDialog(null, "Informe o CEP de Origem");
-		Assert.assertTrue("CEP de Origem não informado", !cepOrigem.isEmpty());
+		assertTrue("Cep de origem foi preenchido", cepOrigem.length() > 0);
 		
 		String cepDestino = JOptionPane.showInputDialog(null, "Informe o CEP de Destino");
-		Assert.assertTrue("CEP de Destino não informado", !cepDestino.isEmpty());
+		assertTrue("Cep de destino foi preenchido", cepDestino.length() > 0);
 		
 		// TODO Adicionar lógica e chamada de services para calcular o frete e exibir
 	}
